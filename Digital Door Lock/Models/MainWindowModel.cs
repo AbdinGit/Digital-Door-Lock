08ad1bdc9ea63d5c1af701fbf47036a5c008cc19
Kicker <Kicker@ABDINDATOR>
2024-10-14 00:02:23 +0200

    Created model for mainwindowview

--- Digital Door Lock/Models/MainWindowModel.cs
+++ Digital Door Lock/Models/MainWindowModel.cs
@@ -8,5 +8,47 @@ namespace Digital_Door_Lock.Models
 {
     public class MainWindowModel
     {
+        private bool _isDoorUnlocked;
+        private readonly IoTHubService _iotHubService;
+
+        public MainWindowModel(IoTHubService iotHubService)
+        {
+            _iotHubService = iotHubService;
+            _isDoorUnlocked = false;
+        }
+
+        public bool IsDoorUnlocked
+        {
+            get => _isDoorUnlocked;
+            private set => _isDoorUnlocked = value;
+        }
+
+        public async Task<string> GetStoredPinAsync()
+        {
+            var twin = await _iotHubService.GetDeviceTwinAsync();
+            if (twin != null && twin.Properties.Desired.Contains("pinCode"))
+            {
+                return twin.Properties.Desired["pinCode"].ToString();
+            }
+            return null;
+        }
+
+        public async Task<bool> ValidatePinAsync(string enteredPin)
+        {
+            var storedPin = await GetStoredPinAsync();
+            return enteredPin == storedPin;
+        }
+
+        public async Task LockDoorAsync()
+        {
+            IsDoorUnlocked = false;
+            await _iotHubService.ReportDeviceStateAsync("Locked");
+        }
+
+        public async Task UnlockDoorAsync()
+        {
+            IsDoorUnlocked = true;
+            await _iotHubService.ReportDeviceStateAsync("Unlocked");
+        }
     }
 }
--- Digital Door Lock/ViewModels/MainWindowViewModel.cs
+++ Digital Door Lock/ViewModels/MainWindowViewModel.cs
@@ -7,14 +7,29 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using Digital_Door_Lock.Models;
 using Digital_Door_Lock.Views;
 
 namespace Digital_Door_Lock.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-
         private string lockImageSource;
+        private string _textBoxContent;
+        private bool _isButtonEnabled = true;
+        private string _currentPin;
+        private MainWindowModel _MainWindowModel;
+
+        public MainWindowViewModel(IoTHubService iotHubService)
+        {
+            _MainWindowModel = new MainWindowModel(iotHubService);
+            LockUnlockCommand = new RelayCommand(ExecuteLockUnlock);
+            ClearDisplayCommand = new RelayCommand(ExecuteClearDisplay);
+            OpenSettingsCommand = new RelayCommand(ExecuteOpenSettings);
+            DisplayPincode();
+            LockImageSource = "../Resource/Images/Locked.png";
+        }
+
         public string LockImageSource
         {
             get => lockImageSource;
@@ -25,19 +40,16 @@ namespace Digital_Door_Lock.ViewModels
             }
         }
 
-        private string _TextBoxContent;
         public string TextBoxContent
         {
-            get { return _TextBoxContent; }
+            get => _textBoxContent;
             set
             {
-                _TextBoxContent = value;
-                OnPropertyChanged("TextBoxContent");
+                _textBoxContent = value;
+                OnPropertyChanged(nameof(TextBoxContent));
             }
         }
 
-
-        private bool _isButtonEnabled = true;
         public bool IsButtonEnabled
         {
             get => _isButtonEnabled;
@@ -51,83 +63,59 @@ namespace Digital_Door_Lock.ViewModels
             }
         }
 
-        private string _CurrentPin;
         public string CurrentPin
         {
-            get { return _CurrentPin; }
+            get => _currentPin;
             set
             {
-                _CurrentPin = value;
-                OnPropertyChanged("CurrentPin");
+                _currentPin = value;
+                OnPropertyChanged(nameof(CurrentPin));
             }
         }
-
-        private bool isDoorUnlocked = false;
 
         public ICommand LockUnlockCommand { get; }
         public ICommand ClearDisplayCommand { get; }
         public ICommand OpenSettingsCommand { get; }
-        private IoTHubService _iotHubService;
-        public MainWindowViewModel(IoTHubService iotHubService)
-        {
-            _iotHubService = iotHubService;
-            LockUnlockCommand = new RelayCommand(ExecuteLockUnlock);
-            ClearDisplayCommand = new RelayCommand(ExecuteClearDisplay);
-            OpenSettingsCommand = new RelayCommand(ExecuteOpenSettings);
-            DisplayPincode();
-            LockImageSource = "../Resource/Images/Locked.png"; 
-        }
+
         private async void DisplayPincode()
         {
             try
             {
-
-                var twin = await _iotHubService.GetDeviceTwinAsync();
-                if (twin != null && twin.Properties.Desired.Contains("pinCode"))
-                {
-                    CurrentPin = $"Pin code is: {twin.Properties.Desired["pinCode"].ToString()}";
-                }
-                else
-                {
-                    CurrentPin = "No PIN found";
-                }
+                var pinCode = await _MainWindowModel.GetStoredPinAsync();
+                CurrentPin = pinCode != null ? $"Pin code is: {pinCode}" : "No PIN found";
             }
-            catch (Exception ex)
+            catch
             {
                 CurrentPin = "Error retrieving PIN";
             }
         }
+
         private async void ExecuteLockUnlock(object sender)
         {
             IsButtonEnabled = false;
-            var iotService = new IoTHubService(PrimaryConnectionString);
 
-            if (isDoorUnlocked)
+            if (_MainWindowModel.IsDoorUnlocked)
             {
                 LockImageSource = "../Resource/Images/Locked.png";
-                await iotService.ReportDeviceStateAsync("Locked");
-                isDoorUnlocked = false;
+                await _MainWindowModel.LockDoorAsync();
                 TextBoxContent = "door locked";
             }
             else
             {
-                bool isPinCorrect = await iotService.ComparePinAsync(TextBoxContent);
-
+                bool isPinCorrect = await _MainWindowModel.ValidatePinAsync(TextBoxContent);
                 if (isPinCorrect)
                 {
                     LockImageSource = "../Resource/Images/Unlocked.png";
-                    await iotService.ReportDeviceStateAsync("Unlocked");
-                    isDoorUnlocked = true;
+                    await _MainWindowModel.UnlockDoorAsync();
                     TextBoxContent = "door unlocked";
                 }
                 else
                 {
                     StartShakingEffect();
-                    await iotService.ReportDeviceStateAsync("Locked");
-                    TextBoxContent = string.Empty;
                     TextBoxContent = "wrong pincode";
                 }
             }
+
             await Task.Delay(3000);
             IsButtonEnabled = true;
         }
@@ -137,14 +125,11 @@ namespace Digital_Door_Lock.ViewModels
             TextBoxContent = string.Empty;
         }
 
-
         private void ExecuteOpenSettings(object sender)
         {
             SettingsWindow settingsWindow = new SettingsWindow();
             settingsWindow.Show();
         }
-
-        private const string PrimaryConnectionString = "HostName=AbdinIOThub.azure-devices.net;DeviceId=cec95419-748a-423a-a9ce-bc9f6c9e6e9a;SharedAccessKey=SeFps8MV4pTBV/5hrcsW28Nlz7/j934q0IiFTBU9XZw=";
 
         private void StartShakingEffect()
         {
@@ -169,20 +154,17 @@ namespace Digital_Door_Lock.ViewModels
                 else
                 {
                     shakeTimer.Stop();
-                    LockImageSource = "../Resource/Images/locked.png"; // Final image
+                    LockImageSource = "../Resource/Images/locked.png";
                 }
             };
 
             shakeTimer.Start();
         }
 
-
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-
     }
 }
